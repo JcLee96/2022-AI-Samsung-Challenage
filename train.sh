9ee2f1b7400ec507e395d42c828200dbddf1253e
Leejucheon96 <78637327+Leejucheon96@users.noreply.github.com>
2023-12-07 20:55:34 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 taskset -c 21-40 python samsung_trainer.py --gpu '3, 4' --nr_epochs 50 --ordinal_class 17 --lr 1.0e-3 --seed 5
+taskset -c 21-40 python samsung_tester.py --gpu '3, 4' --ordinal_class 17 --seed 5
